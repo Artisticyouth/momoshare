1a08f568ae1c57007cf59a65aaba53c2775b6d53
Artisticyouth <47999244+Artisticyouth@users.noreply.github.com>
2023-04-22 21:03:42 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=27335188&pid=9ad048802fb08be5f3bf754ad5c94b93&tid=d65681799d1c7f53a2b9c07623cce6cd",
+    "https://www.maimemo.com/share/page?uid=27335188&pid=d0abafa94945880f2f2c0ba724eda85e&tid=1539f4772328ca88f24ba3a231c415fa",
     
   ]
   /**
